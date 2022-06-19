8c03966458ed14ff485fd7006f9b2746ff947a59
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-19 04:30:18 -0300

    Added: Middleware for validate data

---
+++ src/middleware/validateData.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from 'express';
+// import { httpStatusCode } from '../const/statusCode';
+
+export const validateData = (_req: Request, _res: Response, next: NextFunction) => {
+
+    return next();
+}
