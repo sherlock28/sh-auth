ba22acbb1a0c8850011e40e9eb9982215a96d984
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-06-22 23:31:15 -0300

    Added signup method to AuthController class

--- src/controllers/auth.ctrl.ts
+++ src/controllers/auth.ctrl.ts
@@ -3,7 +3,11 @@ import { HttpStatusCode } from '../const/statusCode';
 
 class AuthController {
     public async signin(_req: Request, res: Response) {
-        res.status(HttpStatusCode.OK).json({ data: 'Hello world', success: true, error: false });
+        res.status(HttpStatusCode.OK).json({ data: 'sign in', success: true, error: false });
+    }
+
+    public async signup(_req: Request, res: Response) {
+        res.status(HttpStatusCode.OK).json({ data: 'sign up', success: true, error: false });
     }
 }
 
