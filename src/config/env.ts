99cb7688848bed8a642a9b45b1683a821df26f00
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-09-06 01:43:42 -0300

    Added: object for export all environment vars

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,8 +1,9 @@
 export const env = {
     PORT: process.env.PORT,
     NODE_ENV: process.env.NODE_ENV,
-    VERSION: process.env.VERSION,
+    VERSION: process.env.VERSION ? +process.env.VERSION : 1,
     APP_DOMAIN: process.env.APP_DOMAIN,
-    GRAPHQL_ENDPOINT: ""+process.env.GRAPHQL_ENDPOINT, 
-    X_HASURA_ADMIN_SECRET: ""+process.env.X_HASURA_ADMIN_SECRET
+    GRAPHQL_ENDPOINT: "" + process.env.GRAPHQL_ENDPOINT,
+    X_HASURA_ADMIN_SECRET: "" + process.env.X_HASURA_ADMIN_SECRET,
+    EXPIRES_IN: process.env.EXPIRES_IN ? +process.env.EXPIRES_IN : 3600
 }
