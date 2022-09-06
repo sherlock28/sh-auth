270dc270ec9dbdaaba2f41f3d5283844e22a6ef4
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2022-09-06 01:44:44 -0300

    Disabling the user registration endpoint

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { authCtrl } from '../controllers/auth.ctrl';
-import { validateRegisterData, validateLoginData } from '../middleware';
+import { validateLoginData } from '../middleware';
 
 const router = Router();
 
-router.post('/signup', validateRegisterData, authCtrl.signup);
+// router.post('/signup', validateRegisterData, authCtrl.signup);
 router.post('/signin', validateLoginData, authCtrl.signin);
 
 export default router;
