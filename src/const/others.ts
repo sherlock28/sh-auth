497568afbb453dd5f388a2e6ae90bd2e924d62f2
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-07-12 02:09:40 -0300

    Fix user category when user sign in with email

---
+++ src/const/others.ts
@@ -0,0 +1,4 @@
+export const USER_CATEGORY = {
+	UNDEFINED_USER_CATEGORY: -1,
+	DEFAULT_USER_CATEGORY: "default"
+}
--- src/libs/generateToken.libs.ts
+++ src/libs/generateToken.libs.ts
@@ -1,18 +1,22 @@
 import jwt from 'jsonwebtoken';
 import { env } from '../config/env';
 import { getPrivKey } from "./getPrivKey.libs";
+import { USER_CATEGORY } from "../const/others";
 
 export const generateToken = (user: { id: number, username: string, email: string, user_category: any }) => {
-    let jwtKey = getPrivKey();
+	let jwtKey = getPrivKey();
 
-    return jwt.sign({
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        "user-category-id": user.user_category.id,
-        "https://hasura.io/jwt/claims": {
-            'x-hasura-default-role': user.user_category.description,
-            'x-hasura-allowed-roles': [user.user_category.description],
-        }
-    }, jwtKey, { algorithm: 'RS256', expiresIn: env.EXPIRES_IN });
+	const categoryId = user.user_category?.id ?? USER_CATEGORY.UNDEFINED_USER_CATEGORY;
+	const categoryDescription = user.user_category?.description ?? USER_CATEGORY.DEFAULT_USER_CATEGORY;
+
+	return jwt.sign({
+		id: user.id,
+		username: user.username,
+		email: user.email,
+		"user-category-id": categoryId,
+		"https://hasura.io/jwt/claims": {
+			'x-hasura-default-role': categoryDescription,
+			'x-hasura-allowed-roles': [categoryDescription],
+		}
+	}, jwtKey, { algorithm: 'RS256', expiresIn: env.EXPIRES_IN });
 }
